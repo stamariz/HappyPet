219c8f87c2a1bec39eb406489ed1efe78a7c98b7
samuel tamariz <stamariz@beyond.pe>
2016-03-31 18:34:10 -0500

    Se realizan mas xml

--- app/src/main/java/com/example/happypet/happypet/MainActivity.java
+++ app/src/main/java/com/example/happypet/happypet/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.happypet.happypet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvSignUp,tvLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSignUp = (TextView)findViewById(R.id.tvSignUp);
+        tvLogIn = (TextView)findViewById(R.id.tvLogIn);
+        tvLogIn.setOnClickListener(onLogInClickListener);
+       tvSignUp.setOnClickListener(onSignInClickListener);
     }
+    View.OnClickListener onLogInClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener onSignInClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
+    };
+
+}
 }
---
+++ app/src/main/java/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constants {
+    public final static String PARAM_NAME = "param_name";
+    public final static String PARAM_APE = "param_apellido";
+    public final static String PARAM_TELE = "param_celular";
+    public final static String PARAM_MAIL = "param_mail";
+    public final static String PARAM_DISTRICT = "param_distrito";
+
+}
